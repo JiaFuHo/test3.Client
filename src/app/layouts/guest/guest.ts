49e742e8eb9dd5a0b863d9887c71923ad311a3a6
JiaFuHo <james890124@gmail.com>
2026-06-28 22:32:57 +0800

    v1.1.1

--- src/app/layouts/guest/guest.ts
+++ src/app/layouts/guest/guest.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { Header } from './header/header';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-guest',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './guest.html',
   styleUrl: './guest.css',
 })
