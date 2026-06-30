3c2b3878790d5b8c062892fd78696102fa2980b7
JiaFuHo <james890124@gmail.com>
2026-06-30 18:12:03 +0800

    v1.2.0

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,27 +1,32 @@
 import { Routes } from '@angular/router';
 
 import { Guest } from './layouts/guest/guest';
-import { Home } from './features/guest/home/home';
+import { LoginG } from './features/guest/login/login';
+import { HomeG } from './features/guest/home/home';
 import { Collection } from './features/guest/collection/collection';
-import { Login } from './features/guest/login/login';
 import { Search } from './features/guest/search/search';
 
+import { Admin } from './layouts/admin/admin';
+import { LoginA } from './features/admin/login/login';
+import { HomeA } from './features/admin/home/home';
+
 export const routes: Routes = [
+  //#region Guest
   {
     path: '',
     component: Guest,
     children: [
       {
+        path: 'login',
+        component: LoginG,
+      },
+      {
         path: 'home',
-        component: Home,
+        component: HomeG,
       },
       {
         path: 'collection',
         component: Collection,
-      },
-      {
-        path: 'login',
-        component: Login,
       },
       {
         path: 'search',
@@ -34,4 +39,27 @@ export const routes: Routes = [
       },
     ],
   },
+  //#endregion
+
+  //#region Admin
+  {
+    path: 'admin',
+    component: Admin,
+    children: [
+      {
+        path: 'login',
+        component: LoginA,
+      },
+      {
+        path: 'home',
+        component: HomeA,
+      },
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full',
+      },
+    ],
+  },
+  //#endregion
 ];
--- src/app/features/admin/login/login.ts
+++ src/app/features/admin/login/login.ts
@@ -6,4 +6,4 @@ import { Component } from '@angular/core';
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class LoginG {}
+export class LoginA {}
